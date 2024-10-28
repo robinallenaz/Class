31500d5b1fb94678d3635a99c97cff42fb2e0f66
Robin Allen <114621789+robinallenaz@users.noreply.github.com>
2024-10-28 10:53:20 -0700

    Added code to redirect to learner path

--- routes/grades.js
+++ routes/grades.js
@@ -5,14 +5,23 @@ import { ObjectId } from "mongodb";
 const router = express.Router();
 
 export default router;
+
+
+
+//Redirecting to learner path
+router.get("/student/:id", (req, res) => {
+    res.redirect(`/learner/${req.params.id}`);
+})
+
 //Base path: /grades
 
 //Get a single grade entry
 
-router.get(":/id", async (req, res) => {
+router.get(":/learner/id", async (req, res) => {
   let collection = await db.collection("grades");
   const query = { _id: new ObjectId(req.params.id) };
   let result = await collection.findOne(query);
 
   if (!result) res.send("Not Found").status(404) else res.send(result).status(200);
-});
+})
+;
